9f3cb0292052b8cec82c599d057cee070d829c69
Drew Dudak <ddudak@gmail.com>
2024-11-22 11:49:44 -0500

    finished the problem, although it could be more effecient

--- 6kyu/Duplicate Encoder/main.js
+++ 6kyu/Duplicate Encoder/main.js
@@ -9,21 +9,54 @@
 // Notes
 // Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 
+// mine. Could be much cleaner -- the object portion isn't efficient. 
 function duplicateEncode(word){
     let holder = {}
     let str = ""
-    let arr = Array.from(word)
+    let arr = Array.from(word.toLowerCase()) // to arr, lowercases string
 
-    arr.forEach(letter => holder[letter] = 0)
-    arr.forEach(letter => holder[letter] += 1)
-    for(let [key,value] of Object.entries(holder)){
-        if (holder[value] > 1){
+    arr.forEach(letter => holder[letter] = 0) // logs existence
+    arr.forEach(letter => holder[letter] += 1) // counts 
+
+    for(let i = 0; i < word.length; i++){ // compares original string to counter obj. records in '(' ')'
+        if (holder[arr[i]] > 1){
             str += ")"
         } else {
             str += "("
         }
-    } console.log(str)
-
+    }return str
 }
 
-duplicateEncode("recede")
+//other great solutions: 
+// functional approach: 
+
+// function duplicateEncode(word){
+//     return word
+//       .toLowerCase()
+//       .split('')
+//       .map( function (a, i, w) {
+//         return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+//       })
+//       .join('');
+//   }
+
+
+////////////////////////////////
+//simplest 
+
+
+// function duplicateEncode(word){
+   
+//     let unique='';
+//     word = word.toLowerCase();
+//     for(const i=0; i<word.length; i++){
+//         if(word.lastIndexOf(word[i]) == word.indexOf(word[i])){
+//             unique += '(';
+//         }
+//         else{
+//             unique += ')';
+//         }
+//     }
+//     return unique;
+
+// }
