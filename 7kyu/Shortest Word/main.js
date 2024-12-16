6b63821532a9d85e8ee3f6b3392b64518f6f8158
Drew Dudak <ddudak@gmail.com>
2024-12-16 15:50:37 -0500

    7kyu array problem

---
+++ 7kyu/Shortest Word/main.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
+
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+    let splitNSort = s.split(" ").sort((a,b) => a.length - b.length)
+    return splitNSort[0].length
+}
+
