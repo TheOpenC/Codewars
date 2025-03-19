a2eed48aa65c3abdcca27e5bc53ee3e477372319
Drew Dudak <ddudak@gmail.com>
2025-03-18 22:26:36 -0400

    7kyu strings

---
+++ 6kyu/Build Tower/main.js
@@ -0,0 +1,14 @@
+
+function towerBuilder(nFloors) {
+    let digits = nFloors * 2 - 1
+        tower = []
+         
+    for(let x = 1; x < nFloors; x++){
+        let stars = x * 2 - 1 * '*'
+        return stars
+        tower.push(stars)
+    }
+    console.log(stars)
+  } 
+
+  towerBuilder(8)
