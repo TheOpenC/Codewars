18f98c4670db55647b03a6e8e72b9bbc625488e1
Drew Dudak <ddudak@gmail.com>
2025-11-11 18:24:48 -0500

    6kyu CodeWars algorithms practice

---
+++ 6kyu/theDeafRats/main.js
@@ -0,0 +1,40 @@
+
+// Story
+
+// The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+
+// But some of the rats are deaf and are going the wrong way!
+// Kata Task
+
+// How many deaf rats are there?
+// Legend
+
+//     P = The Pied Piper
+//     O~ = Rat going left
+//     ~O = Rat going right
+
+// Example
+
+//     ex1 ~O~O~O~O P has 0 deaf rats
+
+//     ex2 P O~ O~ ~O O~ has 1 deaf rat
+
+//     ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
+
+
+// determine the location of P
+//      this tells us which rats are deaf
+// count the deaf rats
+
+
+
+var countDeafRats = function(town) {
+     
+  return 0;
+}
+
+
+countDeafRats('~O~O~O~O P')
+countDeafRats('P O~ O~ ~O O~')
+countDeafRats('~O~O~O~OP~O~OO~')
+
